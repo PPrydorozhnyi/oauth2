baed4f2bfb0fc6764a9d675cd86eb69de460fa96
pepr0718 <petro.prydorozhnyi@netcracker.com>
2019-12-02 15:21:49 +0200

    basic Authorization Server Configuration

---
+++ auth-server/src/main/resources/db/changelog/changelog/sql/clear_schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS oauth_client_details;
+DROP TABLE IF EXISTS oauth_client_token;
+DROP TABLE IF EXISTS oauth_access_token;
+DROP TABLE IF EXISTS oauth_refresh_token;
+DROP TABLE IF EXISTS oauth_code;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS authorities;
