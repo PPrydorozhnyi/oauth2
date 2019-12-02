6e242fa1a32cd8af24ae4e895d52e7126ec03fcc
pepr0718 <petro.prydorozhnyi@netcracker.com>
2019-12-02 16:10:02 +0200

    added Security config

---
+++ auth-server/src/main/java/com/auth/authserver/config/WebSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.auth.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * The @EnableWebSecurity annotation and WebSecurityConfigurerAdapter work together to provide web based security.
+ */
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder;
+
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfiguration(final DataSource dataSource) {
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        userDetailsService = new JdbcDaoImpl();
+        ((JdbcDaoImpl) userDetailsService).setDataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return passwordEncoder;
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+}
